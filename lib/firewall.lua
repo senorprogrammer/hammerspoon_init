0c1342193eade6f63bbcf603970b1e777330c21c
Chris Cummer <chriscummer@me.com>
2017-08-28 10:44:39 -0700

    Remove the local var duplication in firewall.lua
    A bit cleaner code and now the initialization just defaults to logical
    defaults. Still need to refactor the duplication in the callback code.

--- lib/firewall.lua
+++ lib/firewall.lua
@@ -4,36 +4,34 @@
 -- Largely cribbed from https://github.com/cmsj/hammerspoon-config/blob/master/statuslets.lua
 
 local obj = {}
-local interval = 2
+local interval = 5 -- in minutes
 
 obj.timer = nil
 obj.firewallStealthDot = nil
 
+obj.statusDotWidth = 8
+obj.statusDotOffset = 2
+obj.statusDotYVal = 2
+
 hs.canvas.drawingWrapper(true)
 
 function obj:render()
-  local initialScreenFrame = hs.screen.allScreens()[1]:fullFrame()
+  -- Initial positioning. Actual positioning is handled by :reposition()
+  local initialRect = hs.geometry.rect(0, 0, self.statusDotWidth, self.statusDotWidth)
 
-  -- Start off by declaring the size of the text/circle objects and some anchor positions for them on screen
-  local statusDotWidth = 8
-  local statusDotOffset = 2
-  local statusDotYVal = 2
+  self.firewallEnabledDot = hs.drawing.circle(initialRect)
+  self.firewallStealthDot = hs.drawing.circle(initialRect)
 
-  local xPos = self.xPosition(statusDotWidth, statusDotOffset)
+  -- Drawing initialization
+  local labels = {"canJoinAllSpaces", "stationary"}
+  local level = "overlay"
+  local defaultColor = hs.drawing.color.osx_yellow
 
-  self.firewallEnabledDot = hs.drawing.circle(hs.geometry.rect(xPos,
-                                                               self.yPosition(0, statusDotYVal, statusDotWidth, statusDotOffset),
-                                                               statusDotWidth,
-                                                               statusDotWidth))
-  self.firewallStealthDot = hs.drawing.circle(hs.geometry.rect(xPos,
-                                                               self.yPosition(1, statusDotYVal, statusDotWidth, statusDotOffset),
-                                                               statusDotWidth,
-                                                               statusDotWidth))
+  self.firewallEnabledDot:setBehaviorByLabels(labels):setFillColor(defaultColor):setStroke(false):sendToBack():show(0.5)
+  self.firewallStealthDot:setBehaviorByLabels(labels):setFillColor(defaultColor):setStroke(false):sendToBack():show(0.5)
 
-  self.firewallEnabledDot:setBehaviorByLabels({"canJoinAllSpaces", "stationary"}):setFillColor(hs.drawing.color.osx_yellow):setStroke(false):sendToBack():show(0.5)
-  self.firewallEnabledDot:setLevel("overlay")
-  self.firewallStealthDot:setBehaviorByLabels({"canJoinAllSpaces", "stationary"}):setFillColor(hs.drawing.color.osx_yellow):setStroke(false):sendToBack():show(0.5)
-  self.firewallStealthDot:setLevel("overlay")
+  self.firewallEnabledDot:setLevel(level)
+  self.firewallStealthDot:setLevel(level)
 end
 
 function obj.statusletCallbackFirewallEnabled(code, stdout, stderr)
@@ -75,14 +73,11 @@ end
 -- This exists because windowing changes when the screen locks or the screensaver starts
 -- play havoc with the positioning of the elements
 function obj:reposition()
-  local statusDotWidth = 8
-  local statusDotOffset = 2
-  local statusDotYVal = 2
+  -- I stack my dots vertically in the menubar so they have the same X value, and differ in the Y
+  local xPos = self.xPosition(self.statusDotWidth, self.statusDotOffset)
 
-  local xPos = self.xPosition(statusDotWidth, statusDotOffset)
-
-  self.firewallEnabledDot:setTopLeft(hs.geometry.point(xPos, self.yPosition(0, statusDotYVal, statusDotWidth, statusDotOffset)))
-  self.firewallStealthDot:setTopLeft(hs.geometry.point(xPos, self.yPosition(1, statusDotYVal, statusDotWidth, statusDotOffset)))
+  self.firewallEnabledDot:setTopLeft(hs.geometry.point(xPos, self.yPosition(0, self.statusDotYVal, self.statusDotWidth, self.statusDotOffset)))
+  self.firewallStealthDot:setTopLeft(hs.geometry.point(xPos, self.yPosition(1, self.statusDotYVal, self.statusDotWidth, self.statusDotOffset)))
 
   return self
 end
@@ -103,6 +98,7 @@ function obj:start()
   self.timer = hs.timer.new(hs.timer.minutes(interval), function(obj) obj:update() end)
   self.timer:start()
   self:update()
+
   return self
 end
 
@@ -110,6 +106,7 @@ function obj:stop()
   self.timer:stop()
   self.firewallEnabledDot:delete()
   self.firewallStealthDot:delete()
+
   return self
 end
 
